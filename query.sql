7e42555e9f710b01ea240defd7185d527348f123
unknown <maravenables@gmail.com>
2021-03-11 20:59:44 +0800

    postgress db added

---
+++ query.sql
@@ -0,0 +1,16 @@
+-- Query to check successful load
+SELECT * FROM roadworks_details;
+
+-- SELECT * FROM roadworks_ projects;
+
+-- Join tables on x_id
+-- SELECT premise.id, premise.premise_name, county.county_name
+
+-- FROM premise
+-- INNER JOIN county
+-- ON premise.county_id = county.county_id;
+
+-- SELECT tableA.id, tableA.Column1_name, TqbleB.column_name
+-- FROM tableA
+-- INNER JOIN tableB
+-- ON tableA.x_id = tableB.x_id;
---
+++ schema.sql
@@ -0,0 +1,13 @@
+-- Create Two Tables
+CREATE TABLE roadworks_details(
+  Id int PRIMARY KEY,
+  StartDate Date,
+  FinishDate Date,
+  WorkType varchar(250),
+  Suburb varchar(250),
+  Road varchar(250),
+  Region varchar(250),
+  TrafficImpact varchar(250) 
+);
+
+
